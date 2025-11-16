4b99f149175e8258af32efe540cfa7369f95a113
Rafael <rafaelab@users.noreply.github.com>
2025-11-16 21:31:13 +0100

    fix problems for specific options

--- ext/NumberRepresentationMakieRichTextExt.jl
+++ ext/NumberRepresentationMakieRichTextExt.jl
@@ -16,25 +16,32 @@ import NumberRepresentation:
 # ----------------------------------------------------------------------------------------------- #
 #
 NumberRepresentationMakieRichText(repr::NumberRepresentationUnicode{T, U}; timesSymbol::String = "×") where {T, U <:  FixedPointNotation} = begin
-	str = replace(repr.representation, repr.timesSymbol => timesSymbol)
+	if occursin(repr.representation, repr.timesSymbol)
+		str = replace(repr.representation, repr.timesSymbol => timesSymbol)
+	else
+		str = repr.representation
+	end
 	str = rich(str)
 	return NumberRepresentationMakieRichText{T, U, typeof(str)}(repr.number, str, repr.config, timesSymbol)
 end
 
-
 NumberRepresentationMakieRichText(repr::NumberRepresentationUnicode{T, U}; timesSymbol::String = "×") where {T, U <: Union{ScientificNotation, EngineeringNotation}} = begin
 	sig, exp = decomposeNumberFromString(repr.representation, repr.timesSymbol)
-	exp = replace(exp, "10" => "")
-	exp = join(get(superscriptSymbolsDictFrom, c, string(c)) for c ∈ exp) 
-	str = rich(sig, timesSymbol, "10", superscript(exp))
+	if ! isnothing(exp) && occursin("10", exp)
+		exp = replace(exp, "10" => "")
+		exp = join(get(superscriptSymbolsDictFrom, c, string(c)) for c ∈ exp) 
+		str = rich(sig, timesSymbol, "10", superscript(exp))
+	else
+		str = rich(sig)
+	end
+
 	return NumberRepresentationMakieRichText{T, U, typeof(str)}(repr.number, str, repr.config, timesSymbol)
 end
 
-
 NumberRepresentationMakieRichText(number::Real, ::Type{U}, config::NumberRepresentationConfig; timesSymbol::String = "×") where {U <: AbstractNumberNotation} = begin
-	reprU = NumberRepresentationUnicode(number, U, config)
-	return NumberRepresentationMakieRichText(reprU; timesSymbol = timesSymbol)
+	return NumberRepresentationMakieRichText(NumberRepresentationUnicode(number, U, config); timesSymbol = timesSymbol)
 end
+
 
 @buildNumberRepresentationConstructor(NumberRepresentationMakieRichText)
 
